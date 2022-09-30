07fb7ba99f16d356c404ca454f32887b899bb57c
BaileyGreen <baileypgreen12@gmail.com>
2022-09-30 12:40:58 +0200

    Boxed out map

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float forwardInput;
+    private float yawInput;
+    private Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        yawInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = transform.forward * 5 * forwardInput;
+        rigidBody.transform.Rotate(0, yawInput * 2, 0, Space.Self);
+    }
+}
